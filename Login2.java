30402bf77e4197774cbaa312d5d5ec014f78c135
Madhur Bajpai <117451528+BajpaiMADHUR@users.noreply.github.com>
2025-12-03 23:51:12 +0530

    Add files via upload

---
+++ Login2.java
@@ -0,0 +1,9 @@
+package com.x22;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+public class Login2 {
+	public static void main(String[] args) {
+		WebDriver driver2 = new FirefoxDriver();
+		driver2.get("https://xapproach.com");
+	}
+}
---
+++ Login3.java
@@ -0,0 +1,11 @@
+package com.x22;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+public class Login3 {
+
+	public static void main(String[] args) {
+		WebDriver driver3 = new EdgeDriver();
+		driver3.get("https://xapproach.com");
+	}
+
+}
